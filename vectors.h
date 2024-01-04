3b44c87a8887db15711f853b6de2e4979e57dc62
Paulo Castro <p.oliveira.castro@gmail.com>
2024-01-03 22:45:05 -0300

    Make vectors a single-header lib

--- vectors.h
+++ vectors.h
@@ -1,6 +1,10 @@
 #ifndef VECTORS_H
 #define VECTORS_H
 
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
 typedef struct vec3_t {
   float x, y, z;
 } vec3_t;
@@ -25,27 +29,254 @@ static const struct mat4x4_t IDENTITY_M4x4 = {{
     {0.0, 0.0, 0.0, 1.0}
 }};
 
-// Print matrix for debugging
-void printVertex(vec3_t vertex);
-void printMatrix(mat4x4_t matrix);
+// Print for debugging
+static inline void printMatrix(mat4x4_t matrix) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%f ", matrix.data[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+static inline void printVertex(vec3_t vertex) {
+    printf("%f %f %f\n", vertex.x, vertex.y, vertex.z);
+}
 
 // Vector ops
-vec3_t crossProduct(vec3_t a, vec3_t b);
-float dot(vec3_t a, vec3_t b);
-float magnitude(vec3_t v);
-vec3_t sub(vec3_t a, vec3_t b);
-vec3_t add(vec3_t a, vec3_t b);
-vec3_t normalize(vec3_t v);
-vec3_t mulScalarV3(float k, vec3_t v);
+static inline vec3_t crossProduct(vec3_t a, vec3_t b) {
+    vec3_t result;
+
+    result.x = a.y * b.z - a.z * b.y;
+    result.y = a.z * b.x - a.x * b.z;
+    result.z = a.x * b.y - a.y * b.x;
+
+    return result;
+}
+
+static inline float dot(vec3_t a, vec3_t b) {
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+static inline float magnitude(vec3_t v) {
+    float result = sqrt(dot(v, v));
+    assert(result >= 0);
+    return result;
+}
+
+static inline vec3_t sub(vec3_t a, vec3_t b) {
+    return (vec3_t) {a.x - b.x, a.y - b.y, a.z - b.z};
+}
+
+static inline vec3_t add(vec3_t a, vec3_t b) {
+    return (vec3_t) {a.x + b.x, a.y + b.y, a.z + b.z};
+}
+
+static inline vec3_t normalize(vec3_t v) {
+    float mag = magnitude(v);
+    return (vec3_t) {v.x / mag, v.y / mag, v.z / mag};
+}
 
 // Matrix ops
-mat4x4_t transposeM4(mat4x4_t m);
-mat4x4_t inverseM4(mat4x4_t matrix);
-vec4_t mulMV4(mat4x4_t mat4x4, vec4_t vec4);
-vec3_t mulMV3(mat4x4_t mat4x4, vec3_t v);
-mat4x4_t mulMM4(mat4x4_t m1, mat4x4_t m2);
+static inline vec3_t mulScalarV3(float k, vec3_t v) {
+    return (vec3_t) {k*v.x, k*v.y, k*v.z};
+}
+
+static inline mat4x4_t transposeM4(mat4x4_t m) {
+    mat4x4_t result = {0};
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            result.data[i][j] = m.data[j][i];
+        }
+    }
+    return result;
+}
+
+// Function to calculate the determinant of a 3x3 matrix
+static inline float determinant(float a, float b, float c, float d, float e, float f, float g, float h, float i) {
+    return a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
+}
+
+// Function to calculate the inverse of a matrix
+static inline mat4x4_t inverseM4(mat4x4_t matrix) {
+    float m[16];
+    for (int i = 0; i < 16; i++) {
+        m[i] = matrix.data[i/4][i%4];
+    }
+
+    float invOut[16];
+    float inv[16], det;
+    int i;
+
+    inv[0] = m[5]  * m[10] * m[15] - 
+             m[5]  * m[11] * m[14] - 
+             m[9]  * m[6]  * m[15] + 
+             m[9]  * m[7]  * m[14] +
+             m[13] * m[6]  * m[11] - 
+             m[13] * m[7]  * m[10];
+
+    inv[4] = -m[4]  * m[10] * m[15] + 
+              m[4]  * m[11] * m[14] + 
+              m[8]  * m[6]  * m[15] - 
+              m[8]  * m[7]  * m[14] - 
+              m[12] * m[6]  * m[11] + 
+              m[12] * m[7]  * m[10];
+
+    inv[8] = m[4]  * m[9] * m[15] - 
+             m[4]  * m[11] * m[13] - 
+             m[8]  * m[5] * m[15] + 
+             m[8]  * m[7] * m[13] + 
+             m[12] * m[5] * m[11] - 
+             m[12] * m[7] * m[9];
+
+    inv[12] = -m[4]  * m[9] * m[14] + 
+               m[4]  * m[10] * m[13] +
+               m[8]  * m[5] * m[14] - 
+               m[8]  * m[6] * m[13] - 
+               m[12] * m[5] * m[10] + 
+               m[12] * m[6] * m[9];
+
+    inv[1] = -m[1]  * m[10] * m[15] + 
+              m[1]  * m[11] * m[14] + 
+              m[9]  * m[2] * m[15] - 
+              m[9]  * m[3] * m[14] - 
+              m[13] * m[2] * m[11] + 
+              m[13] * m[3] * m[10];
+
+    inv[5] = m[0]  * m[10] * m[15] - 
+             m[0]  * m[11] * m[14] - 
+             m[8]  * m[2] * m[15] + 
+             m[8]  * m[3] * m[14] + 
+             m[12] * m[2] * m[11] - 
+             m[12] * m[3] * m[10];
+
+    inv[9] = -m[0]  * m[9] * m[15] + 
+              m[0]  * m[11] * m[13] + 
+              m[8]  * m[1] * m[15] - 
+              m[8]  * m[3] * m[13] - 
+              m[12] * m[1] * m[11] + 
+              m[12] * m[3] * m[9];
+
+    inv[13] = m[0]  * m[9] * m[14] - 
+              m[0]  * m[10] * m[13] - 
+              m[8]  * m[1] * m[14] + 
+              m[8]  * m[2] * m[13] + 
+              m[12] * m[1] * m[10] - 
+              m[12] * m[2] * m[9];
+
+    inv[2] = m[1]  * m[6] * m[15] - 
+             m[1]  * m[7] * m[14] - 
+             m[5]  * m[2] * m[15] + 
+             m[5]  * m[3] * m[14] + 
+             m[13] * m[2] * m[7] - 
+             m[13] * m[3] * m[6];
+
+    inv[6] = -m[0]  * m[6] * m[15] + 
+              m[0]  * m[7] * m[14] + 
+              m[4]  * m[2] * m[15] - 
+              m[4]  * m[3] * m[14] - 
+              m[12] * m[2] * m[7] + 
+              m[12] * m[3] * m[6];
+
+    inv[10] = m[0]  * m[5] * m[15] - 
+              m[0]  * m[7] * m[13] - 
+              m[4]  * m[1] * m[15] + 
+              m[4]  * m[3] * m[13] + 
+              m[12] * m[1] * m[7] - 
+              m[12] * m[3] * m[5];
+
+    inv[14] = -m[0]  * m[5] * m[14] + 
+               m[0]  * m[6] * m[13] + 
+               m[4]  * m[1] * m[14] - 
+               m[4]  * m[2] * m[13] - 
+               m[12] * m[1] * m[6] + 
+               m[12] * m[2] * m[5];
+
+    inv[3] = -m[1] * m[6] * m[11] + 
+              m[1] * m[7] * m[10] + 
+              m[5] * m[2] * m[11] - 
+              m[5] * m[3] * m[10] - 
+              m[9] * m[2] * m[7] + 
+              m[9] * m[3] * m[6];
+
+    inv[7] = m[0] * m[6] * m[11] - 
+             m[0] * m[7] * m[10] - 
+             m[4] * m[2] * m[11] + 
+             m[4] * m[3] * m[10] + 
+             m[8] * m[2] * m[7] - 
+             m[8] * m[3] * m[6];
+
+    inv[11] = -m[0] * m[5] * m[11] + 
+               m[0] * m[7] * m[9] + 
+               m[4] * m[1] * m[11] - 
+               m[4] * m[3] * m[9] - 
+               m[8] * m[1] * m[7] + 
+               m[8] * m[3] * m[5];
+
+    inv[15] = m[0] * m[5] * m[10] - 
+              m[0] * m[6] * m[9] - 
+              m[4] * m[1] * m[10] + 
+              m[4] * m[2] * m[9] + 
+              m[8] * m[1] * m[6] - 
+              m[8] * m[2] * m[5];
+
+    det = m[0] * inv[0] + m[1] * inv[4] + m[2] * inv[8] + m[3] * inv[12];
+
+    if (det == 0)
+        return IDENTITY_M4x4;
+
+    det = 1.0 / det;
+
+    for (i = 0; i < 16; i++)
+        invOut[i] = inv[i] * det;
+    
+    mat4x4_t invMatrix;
+    for (int i = 0; i < 4; i++) {
+        invMatrix.data[i][0] = invOut[i*4];
+        invMatrix.data[i][1] = invOut[i*4+1];
+        invMatrix.data[i][2] = invOut[i*4+2];
+        invMatrix.data[i][3] = invOut[i*4+3];
+    }
+
+    return invMatrix;
+}
+
+static inline vec4_t mulMV4(mat4x4_t mat4x4, vec4_t vec4) {
+  float result[4] = {0};
+
+  for (int i = 0; i < 4; i++) {
+    result[i] += mat4x4.data[i][0]*vec4.x;
+    result[i] += mat4x4.data[i][1]*vec4.y;
+    result[i] += mat4x4.data[i][2]*vec4.z;
+    result[i] += mat4x4.data[i][3]*vec4.w;
+  }
+
+  return (vec4_t) {result[0], result[1], result[2], result[3]};
+}
+
+static inline vec3_t mulMV3(mat4x4_t mat4x4, vec3_t v) {
+    vec4_t vec4 = {v.x, v.y, v.z, 1};
+    vec4_t result = mulMV4(mat4x4, vec4);
+    return (vec3_t) {result.x, result.y, result.z};
+}
+
+static inline mat4x4_t mulMM4(mat4x4_t m1, mat4x4_t m2) {
+    mat4x4_t result = {0};
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            for (int k = 0; k < 4; k++) {
+                result.data[i][j] += m1.data[i][k]*m2.data[k][j];
+            }
+        }
+    }
+
+    return result;
+}
 
 // Plane ops
-float distancePlaneV3(plane_t plane, vec3_t v);
+static inline float distancePlaneV3(plane_t plane, vec3_t v) {
+    return dot(plane.normal, v) + plane.distance;
+}
 
 #endif
